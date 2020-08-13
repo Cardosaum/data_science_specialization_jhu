8ceca384cb9de51a9f88edecf06057a39e901ec1
Cardosaum <mcardosos99@gmail.com>
2020-08-12 21:02:46 -0300

    finished app
    processed data and now the analysis is sharable.

--- 09_developing_data_products/week4/app.R
+++ 09_developing_data_products/week4/app.R
@@ -10,12 +10,15 @@ library(leaflet) # for interactive maps
 library(ggplot2) # tidyverse data visualization package
 library(broom)
 library(magrittr)
+library(forcats)
+enem_df <- read_rds("data/enem.rds")
+xtext <- "Mathematics"
 
 # Define UI for app that draws a histogram ----
 ui <- fluidPage(
 
     # App title ----
-    titlePanel("Notas Enem"),
+    titlePanel("Analysis of Score in Enem - Brazil's National Exam for High School Education"),
 
     # Sidebar layout with input and output definitions ----
     sidebarLayout(
@@ -24,13 +27,31 @@ ui <- fluidPage(
         sidebarPanel(
 
             selectInput(inputId = "tipo_nota",
-                        label = "Selecione qual nota mostrar",
+                        label = "Choose Which Subject to Plot",
                         choices = c(
-                                    "Redação" = "NU_NOTA_REDACAO",
-                                    "Matemática" = "NU_NOTA_MT",
-                                    "Linguaguens" = "NU_NOTA_LC",
-                                    "Natureza" = "NU_NOTA_CN",
-                                    "Humanas" = "NU_NOTA_CH"))
+                                    "Mathematics" = "NU_NOTA_MT",
+                                    "Essay" = "NU_NOTA_REDACAO",
+                                    "Portuguese" = "NU_NOTA_LC",
+                                    "Natural Sciences (Chemistry/Physics/Biology)" = "NU_NOTA_CN",
+                                    "Human Sciences (History/Geograpy/Filosophy/Sociology)" = "NU_NOTA_CH")),
+            selectInput(inputId = "wrap_var",
+                        label = "Facet Plot by which variable?",
+                        choices = c("School Type" = "TP_ESCOLA",
+                                    "Foreing Language (English/Spanish)" = "TP_LINGUA",
+                                    "Race/Ethnicity" = "TP_COR_RACA",
+                                    "Marital Status" = "TP_ESTADO_CIVIL",
+                                    "State" = "SG_UF_RESIDENCIA",
+                                    "Age" = "NU_IDADE",
+                                    "Male/Female" = "TP_SEXO")),
+            selectInput(inputId = "facet_var",
+                        label = "Color Plot by which variable?",
+                        choices = c("Male/Female" = "TP_SEXO",
+                                    "Foreing Language (English/Spanish)" = "TP_LINGUA",
+                                    "Race/Ethnicity" = "TP_COR_RACA",
+                                    "Marital Status" = "TP_ESTADO_CIVIL",
+                                    "State" = "SG_UF_RESIDENCIA",
+                                    "Age" = "NU_IDADE",
+                                    "School Type" = "TP_ESCOLA"))
 
         ),
 
@@ -39,8 +60,9 @@ ui <- fluidPage(
 
             plotOutput(outputId = "enem_plot"),
             br(),
-            textOutput("materia_selecionada"),
             br(),
+            h4("Map with number of students per State"),
+            h6("Only showing those who could ingress in the Computer Science graduation course in University of São Paulo (USP) with their grades."),
             tmapOutput("map"),
             br(),
             br()
@@ -54,8 +76,6 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram ----
 server <- function(input, output) {
 
-    enem_df <- read_rds("data/os_bons.rds")
-    
     brazil <- st_read("data/gis-dataset-brasil/uf/geojson/uf.json")
     
     brazil %<>% 
@@ -82,15 +102,33 @@ server <- function(input, output) {
     #                     "Linguaguens" = "NU_NOTA_LC",
     #                     "Natureza" = "NU_NOTA_CN",
     #                     "Humanas" = "NU_NOTA_CH")
-
+    # xlab_text <- observe({
+    # if (input$tipo_nota == "NU_NOTA_REDACAO") {
+    #     xtext <- "Essay"
+    # } else if (input$tipo_nota == "NU_NOTA_MT") {
+    #     xtext <- "Mathematics"
+    # } else if (input$tipo_nota == "NU_NOTA_CN") {
+    #     xtext <- "Natural Sciences (Chemistry/Physics/Biology)"
+    # } else if (input$tipo_nota == "NU_NOTA_CH") {
+    #     xtext <- "Human Sciences (History/Geograpy/Filosophy/Sociology)"
+    # } else if (input$tipo_nota == "NU_NOTA_LC") {
+    #     xtext <- "Portuguese"
+    # } else {
+    #     xtext <- "ERROR"
+    # }
+    #     print(xtext)
+    # })
     output$enem_plot <- renderPlot({
-        enem_df %>%
+        enem_df %>% 
             ggplot() +
-            geom_histogram(aes(.data[[input$tipo_nota]], fill = me))
+            geom_histogram(aes(.data[[input$tipo_nota]], fill = .data[[input$facet_var]])) +
+            facet_wrap(~ .data[[input$wrap_var]]) +
+            labs(title = "Histogram of scores in Enem",
+                 x = "Score (0 - 1000)",
+                 caption = "Only showing the students who could ingress in Computer Science at São Paulo University (USP) using their scores.") +
+            theme(legend.title = element_blank())
     })
 
-    output$materia_selecionada <- renderText({input$tipo_nota})
-    
     output$map <- renderTmap({
         enem_df %>% 
             group_by(SG_UF_RESIDENCIA) %>% 
